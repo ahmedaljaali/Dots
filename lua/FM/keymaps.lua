bee2bcde820c0be5e50ee844160fdc60fd9b25c7
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-03 23:21:09 +0300

    Add touble.nvim

--- lua/FM/keymaps.lua
+++ lua/FM/keymaps.lua
@@ -70,3 +70,14 @@ map('n', '<leader>tr', ':GTestRun<cr>', opts)
 map('n', '<leader>cg', ':CMakeGenerate<cr>', opts)
 map('n', '<leader>cb', ':CMakeBuild<cr>', opts)
 --------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------
+--trouble
+-- Lua
+map("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
+map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
+map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
+map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
+map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
+map("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
+--------------------------------------------------------------------------------------------------
