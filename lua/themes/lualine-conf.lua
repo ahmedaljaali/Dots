86c19e10c87afac14e83e49c6c05b4acbe7ae0ac
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-03 17:20:29 +0300

    Add lualine-conf

---
+++ lua/themes/lualine-conf.lua
@@ -0,0 +1,39 @@
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype', 'filesize', 'tabs'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+------------------------------------------------------
+--Always don't show tabs
+vim.cmd[[set showtabline=0]]
+
+------------------------------------------------------
+
+------------------------------------------------------
+--We don't need to see things like -- INSERT -- anymore
+vim.opt.showmode = false
+------------------------------------------------------
