6b845b489463e1336ca60c5800d72708a4436f80
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-08 21:03:18 +0300

    Add cmp-lua

--- lua/language_support/cmp-config.lua
+++ lua/language_support/cmp-config.lua
@@ -1,26 +1,20 @@
   local cmp = require'cmp'
 
   cmp.setup({
-        mapping = {
+    mapping = {
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<C-y>'] = cmp.config.disable,
+      ['<C-e>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
+      { name = 'nvim_lua' },
       { name = 'buffer' },
     })
+
   })
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
@@ -38,22 +32,3 @@
       { name = 'cmdline' }
     })
   })
-
-  -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['clangd'].setup {
-    capabilities = capabilities
-  }
-
-  require('lspconfig')['cmake'].setup {
-    capabilities = capabilities
-  }
-
-  require('lspconfig')['sumneko_lua'].setup {
-    capabilities = capabilities
-  }
-
-  require('lspconfig')['vimls'].setup {
-    capabilities = capabilities
-  }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -168,7 +168,8 @@ return packer.startup(function()
   {
     'hrsh7th/nvim-cmp',
     requires = {{'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
-                'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-nvim-lsp',}}
+                'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/cmp-nvim-lua'}}
   }
 ---------------------------------------------------------
 
