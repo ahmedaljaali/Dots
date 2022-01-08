d7467a09f3cbe8965d00518ff3f6563eb639d91b
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-08 21:26:54 +0300

    Change the build directory from Debug to Build

--- lua/language_support/cmake-config.lua
+++ lua/language_support/cmake-config.lua
@@ -1,10 +1,10 @@
 -----------------------------------------------
 --excute compile command
 vim.g.cmake_link_compile_commands = true
+vim.g.cmake_default_config = 'Build'
 -----------------------------------------------
 
 -----------------------------------------------
 --give it the path to the executable and run it
-vim.g['gtest#gtest_command'] = 'Debug/app'
+vim.g['gtest#gtest_command'] = 'Build/app'
 -----------------------------------------------
-
