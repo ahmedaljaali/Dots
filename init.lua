507a268c86caa21692cb9ebf7cfef659024b0810
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-13 10:39:14 +0300

    manage ini.lua

--- init.lua
+++ init.lua
@@ -2,18 +2,12 @@ require('plugins') --Put it in the top of the file, trust me you don't want to c
 require('general')
 
 ----------------------------------------------------------------------
---Language Support
+--Fast moving
 
-require('language_support/cmake-config')
-require('language_support/treesitter-config')
-require('language_support/lsp-installer')
-require('language_support/cmp-config')
-require('language_support/gitsigns')
-require('language_support/lspkind')
-require('language_support/lsp-config')
-require('language_support/diagnostics')
-require('language_support/code-actions')
-require('language_support/nvim-lua')
+require('FM/start-screan')
+require('FM/fzfz')
+require('FM/fileStorm')
+require('FM/keymaps')
 ----------------------------------------------------------------------
 
 ----------------------------------------------------------------------
@@ -26,20 +20,25 @@ require('themes/icons')
 ----------------------------------------------------------------------
 
 ----------------------------------------------------------------------
---Fast moving
-
-require('FM/start-screan')
-require('FM/fzfz')
-require('FM/fileStorm')
-require('FM/keymaps')
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
 --typing
 
 require('typing/indent')
 require('typing/spelling')
 require('typing/autopairs')
 require('typing/comment-out')
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+--Language Support
+
+require('language_support/cmake-config')
+require('language_support/treesitter-config')
+require('language_support/lsp-installer')
+require('language_support/cmp-config')
+require('language_support/gitsigns')
+require('language_support/lspkind')
+require('language_support/lsp-config')
+require('language_support/diagnostics')
+require('language_support/code-actions')
+require('language_support/nvim-lua')
 ----------------------------------------------------------------------
