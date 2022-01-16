312552b37bc911826799600137b3742c3999b204
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-16 19:13:00 +0300

    Change the hight of the flouterm

--- lua/FM/fileStorm.lua
+++ lua/FM/fileStorm.lua
@@ -5,5 +5,5 @@ vim.g.floaterm_opener = 'tabe'
 vim.g.floaterm_autoclose = 2
 vim.g.floaterm_autohide = 2
 vim.g.floaterm_width = 0.9
-vim.g.floaterm_height = 0.9
+vim.g.floaterm_height = 0.4
 vim.g.floaterm_wintype = 'split'
