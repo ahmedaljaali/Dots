93e344f2e4d564982dd50a48b73d47a57cbfdc52
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-17 19:34:10 +0300

    Add cmp-under-comparator

--- lua/language_support/cmp-config.lua
+++ lua/language_support/cmp-config.lua
@@ -66,6 +66,18 @@ end
       },
     },
   },
+      sorting = {
+        comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            require "cmp-under-comparator".under,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
+    },
 
   })
 
