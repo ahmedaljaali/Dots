acdc7ef1c837fa849247af696c18ebdd260fac7f
Aqashir <ahmedmohammed2429@gmail.com>
2022-01-24 14:12:30 +0300

    Update

--- lua/FM/keymaps.lua
+++ lua/FM/keymaps.lua
@@ -74,7 +74,8 @@ vim.cmd[[
     tnoremap <C-l> <C-\><C-N><C-w>l
     tnoremap <Leader>ct <C-\><C-n>
 ]]
-map('n', '<Leader>db', ':terminal gdb Build/app<CR>', opts) --open terminal
+map('n', '<Leader>db', ':tabe term://gdb Build app<CR>', opts) --open terminal with gdb
+
 --------------------------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------------------------
