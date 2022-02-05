4db1502a366d2e3d907b9b4599fea16531271f47
Aqashir <ahmedmohammed2429@gmail.com>
2022-02-05 19:12:05 +0300

    Add emojis

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,10 +40,10 @@ packer.init
     non_interactive = false, -- If true, disable display windows for all operations
     open_fn  = nil, -- An optional function to open a window for packer's display
     open_cmd = '65vnew \\[packer\\]', -- An optional command to open a window for packer's display
-    working_sym = '', -- The symbol for a plugin being installed/updated
-    error_sym = '✗', -- The symbol for a plugin with an error in installation/updating
-    done_sym = '✓', -- The symbol for a plugin which has completed installation/updating
-    removed_sym = '-', -- The symbol for an unused plugin which was removed
+    working_sym = '⏲️ ', -- The symbol for a plugin being installed/updated
+    error_sym = '😞', -- The symbol for a plugin with an error in installation/updating
+    done_sym = '👍', -- The symbol for a plugin which has completed installation/updating
+    removed_sym = '👋', -- The symbol for an unused plugin which was removed
     moved_sym = '→', -- The symbol for a plugin which was moved (e.g. from opt to start)
     header_sym = '━', -- The symbol for the header line in packer's display
     show_all_info = true, -- Should packer show all update details automatically?
