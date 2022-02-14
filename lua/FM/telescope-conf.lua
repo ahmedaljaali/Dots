7c03ed7345989b74179192d48d466ff046173e91
Aqashir <ahmedmohammed2429@gmail.com>
2022-02-14 21:12:51 +0300

    Telescope will ignore Build and Debug dirs

--- lua/FM/telescope-conf.lua
+++ lua/FM/telescope-conf.lua
@@ -37,7 +37,7 @@ telescope.setup {
          preview_cutoff = 120,
       },
       file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = { "node_modules" },
+      file_ignore_patterns = { "node_modules", "Build", "Debug" },
       generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
       path_display = { "truncate" },
       winblend = 0,
