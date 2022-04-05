1e64b4814f0a3f5e187d955276f9b5bcb26e3047
Aqashir <ahmedmohammed2429@gmail.com>
2022-04-05 17:17:52 +0300

    Fix

--- lua/FM/keymaps.lua
+++ lua/FM/keymaps.lua
@@ -275,6 +275,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
