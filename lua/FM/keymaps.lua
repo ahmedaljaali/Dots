1a497fdfe6c8e088d08fdfdd8707701bf3208187
Aqashir <ahmedmohammed2429@gmail.com>
2022-04-14 18:29:36 +0300

    currect indentation

--- lua/FM/keymaps.lua
+++ lua/FM/keymaps.lua
@@ -258,7 +258,7 @@ telescope.setup {
 cmp.setup({
     mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
