a0ad7f9f16042694f013d5492bf76c7ae1d31402
Aqashir <ahmedmohammed2429@gmail.com>
2022-04-19 21:26:27 +0300

    add snippet to build shaders

--- lua/snippets/cmake.lua
+++ lua/snippets/cmake.lua
@@ -38,4 +38,43 @@ ls.add_snippets(nil, {
     "#-Werror add this option if you want to treat warnings as errors"
   })
     }),
+        snip({
+
+            trig = "shaders",
+            namr = "Shaders",
+            dscr = "Build SHADERS"
+        },
+        {
+        text({"find_program(GLSL_VALIDATOR glslangValidator HINTS",
+        "  ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}",
+        "  /usr/bin",
+        "  /usr/local/bin",
+        "  ${VULKAN_SDK_PATH}/Bin",
+        "  ${VULKAN_SDK_PATH}/Bin32",
+        "  $ENV{VULKAN_SDK}/Bin/",
+        "  $ENV{VULKAN_SDK}/Bin32/",
+        "",
+        ")",
+        "# get all .vert and .frag files in shaders directory",
+        "file(GLOB_RECURSE GLSL_SOURCE_FILES",
+        '\"${PROJECT_SOURCE_DIR}/shaders/*.frag\"',
+        '\"${PROJECT_SOURCE_DIR}/shaders/*.vert\"',
+        ")",
+        "foreach(GLSL ${GLSL_SOURCE_FILES})",
+        "  get_filename_component(FILE_NAME ${GLSL} NAME)",
+        '  set(SPIRV \"${PROJECT_SOURCE_DIR}/shaders/${FILE_NAME}.spv\")',
+        "  add_custom_command(",
+        "    OUTPUT ${SPIRV}",
+        "    COMMAND ${GLSL_VALIDATOR} -V ${GLSL} -o ${SPIRV}",
+        "    DEPENDS ${GLSL})",
+        "  list(APPEND SPIRV_BINARY_FILES ${SPIRV})",
+        "endforeach(GLSL)",
+        "",
+        "add_custom_target(",
+        "    Shaders",
+        "    DEPENDS ${SPIRV_BINARY_FILES}",
+        ")",
+        "add_dependencies(${PROJECT_NAME} Shaders)"
+        })
+    })
 }})
