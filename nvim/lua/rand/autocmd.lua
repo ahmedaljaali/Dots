d3d52e9741479d0d3e49a3d20ed92731cc3fd47b
Aqashir <ahmedmohammed2429@gmail.com>
2022-04-23 18:26:48 +0300

    Nvim: Edit autocmd

--- nvim/lua/rand/autocmd.lua
+++ nvim/lua/rand/autocmd.lua
@@ -1,5 +1,7 @@
 -----------------------------------------------------
 --Open .vert and .frag file as .glsl files
-vim.cmd([[autocmd BufNewFile,BufRead  *.frag set filetype=glsl]])
-vim.cmd([[autocmd BufNewFile,BufRead  *.vert set filetype=glsl]])
+vim.api.nvim_create_autocmd(
+    { "BufNewFile", "BufRead" },
+    { pattern = { "*.vert", "*.frag"}, command = ":set filetype=glsl" }
+)
 -----------------------------------------------------
