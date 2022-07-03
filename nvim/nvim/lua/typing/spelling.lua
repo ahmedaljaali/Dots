276e807d7445a8391875c3d24f1d29ab268dd6ad
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-03 18:41:06 +0300

    Nvim: Edit spelling settings

--- nvim/nvim/lua/typing/spelling.lua
+++ nvim/nvim/lua/typing/spelling.lua
@@ -1,11 +1,5 @@
-vim.opt.spell = true
-vim.opt.spelllang = { 'en_us' }
-
 require('spellsitter').setup {
   -- Whether enabled, can be a list of file types, e.g. {'python', 'lua'}
   enable = true,
-  code_spellcheck = false, -- enable additional code spellchecks
-  naming_styles = {'camel_case', 'snake_case'}  -- how to parse names to separate words
+  debug = false
 }
-
-vim.cmd[[set nospell filetype=terminal]] --disable it while using terminal
