2239e6ae274c7b13f8019c00d4a542a0fd222e99
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-05 13:34:08 +0300

    Dwm: makes horizontal and vertical padding in the status bar into configurable options

--- programs/dwm/config.def.h
+++ programs/dwm/config.def.h
@@ -7,6 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 0;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "MesloLGS Nerd Font:size=10" };
 static const char dmenufont[]       = "MesloLGS Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
