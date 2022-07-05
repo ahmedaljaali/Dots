40c99507873e23f3ccd96988328087b84b4ee0cd
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-05 12:44:26 +0300

    Dwm: you can move windows now

--- programs/dwm/config.def.h
+++ programs/dwm/config.def.h
@@ -1,3 +1,5 @@
+#include "movestack.c"
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -81,6 +83,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
