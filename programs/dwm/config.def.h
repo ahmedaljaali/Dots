609f838c3317efc667565caae0b09d8784b1e92a
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-05 14:30:27 +0300

    Dwm: change status bar color

--- programs/dwm/config.def.h
+++ programs/dwm/config.def.h
@@ -11,11 +11,11 @@ static const int horizpadbar        = 2;        /* horizontal padding for status
 static const int vertpadbar         = 0;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "MesloLGS Nerd Font:size=10" };
 static const char dmenufont[]       = "MesloLGS Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#3c3836";
+static const char col_gray2[]       = "#d3869b";
+static const char col_gray3[]       = "#f2e5bc";
+static const char col_gray4[]       = "#98971a";
+static const char col_cyan[]        = "#3c3836";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
