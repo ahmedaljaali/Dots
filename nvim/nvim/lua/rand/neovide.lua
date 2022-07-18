c8e5bc8d5a434f4c68a1e6b82ccc54c275ce2a73
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-18 19:19:45 +0300

    Nvim: edit neovide config

--- nvim/nvim/lua/rand/neovide.lua
+++ nvim/nvim/lua/rand/neovide.lua
@@ -1,7 +1,19 @@
+----------------------------------------------------------------------
+--        Everything here will be executed only with neovide        --
+
 vim.cmd([[
 if exists("g:neovide")
-    " Put anything you want to happen only in Neovide here
     let g:neovide_fullscreen=v:true
+
     set guifont=MesloLGS\ Nerd\ Font:h13
+
+    let g:neovide_cursor_animation_length=0.2
+
+    let g:neovide_cursor_vfx_mode = "pixiedust"
+
+    let g:neovide_cursor_vfx_particle_lifetime=5
+
+    let g:neovide_cursor_vfx_particle_density=14.0
 endif
 ]])
+----------------------------------------------------------------------
