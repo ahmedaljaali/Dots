a5704a061afe56d40cc546fa4a5eff8f4ab6e184
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-18 23:01:53 +0300

    Nvim: change icon color for code files that ends with .cc

--- nvim/nvim/lua/colors/icons.lua
+++ nvim/nvim/lua/colors/icons.lua
@@ -1,10 +1,14 @@
-require'nvim-web-devicons'.setup {
+local webdev = require'nvim-web-devicons'
 
- override = {
-
-
- };
-
-
- default = true;
+local my_icons = {
+      cc= {
+        icon = "",
+        color = "#458588",
+        name = "cc"
+      }
 }
+if webdev.has_loaded then
+  webdev.set_icon (my_icons)
+else
+  webdev.setup({ override = my_icons, default = true })
+end
