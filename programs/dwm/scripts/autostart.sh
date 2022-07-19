891f2cb141f513c069cd2f4b22ed966bec44be5e
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-19 15:10:01 +0300

    Dwm: Update xidlehook command

--- programs/dwm/scripts/autostart.sh
+++ programs/dwm/scripts/autostart.sh
@@ -10,7 +10,7 @@ picom --experimental-backends &
 betterlockscreen -u ~/wallpapers/Sunset.png --dim 40 --blur 1 &
 
 #lock screen
-xidlehook --not-when-fullscreen --timer 60 'light -S 30' 'light -S 50' --timer 10  "notify-send -u critical -t 10000 'LOCKING THE SCREEN IN 30 SECONDS'"  "light -S 50&&notify-send -u critical -t 10000 'Cancelled'" --timer 30 "betterlockscreen -l dimblur" "light -S 50"  --timer 300 'systemctl suspend' 'light -S 50' &
+xidlehook  --not-when-audio --timer 60 'light -S 30' 'light -S 50' --timer 10  "notify-send -u critical -t 10000 'LOCKING THE SCREEN IN 30 SECONDS'"  "light -S 50&&notify-send -u critical -t 10000 'Cancelled'" --timer 30 "betterlockscreen -l dimblur" "light -S 50"  --timer 300 'systemctl suspend' 'light -S 50' &
 
 # NetworkManager is the most popular way to manage wireless networks on Linux,
 # and nm-applet is a desktop environment-independent system tray GUI for it.
