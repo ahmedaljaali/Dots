f8a0be0c60b50a011c274181a306fc4643ded9d7
IZenithy <ahmedmohammed2429@gmail.com>
2022-07-20 23:57:36 +0300

    Nvim: Neoclip can use sqlite

--- nvim/nvim/lua/typing/neoclip-conf.lua
+++ nvim/nvim/lua/typing/neoclip-conf.lua
@@ -1,6 +1,6 @@
 require('neoclip').setup({
     history = 1000,
-    enable_persistent_history = false,
+    enable_persistent_history = true,
     length_limit = 1048576,
     continuous_sync = false,
     db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
