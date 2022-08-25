4d537a93934babf914131191255ec84ed3f82dee
IZenithy <ahmedmohammed2429@gmail.com>
2022-08-25 17:03:45 +0300

    Nvim: Edit cmake snippet

--- nvim/nvim/lua/snippets/cmake.lua
+++ nvim/nvim/lua/snippets/cmake.lua
@@ -57,7 +57,6 @@ target_include_directories(${{PROJECT_NAME}} PUBLIC ${{CMAKE_CURRENT_SOURCE_DIR}
 
 
 set_target_properties(${{PROJECT_NAME}} PROPERTIES
-
     # Specify directories
     ARCHIVE_OUTPUT_DIRECTORY '${{CMAKE_CURRENT_SOURCE_DIR}}/lib'
     LIBRARY_OUTPUT_DIRECTORY '${{CMAKE_CURRENT_SOURCE_DIR}}/lib'
@@ -119,30 +118,30 @@ local shaders = s('shaders',
 
 
 find_program(GLSL_VALIDATOR glslangValidator HINTS
-  ${{Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}}
-  /usr/bin
-  /usr/local/bin
-  ${{VULKAN_SDK_PATH}}/Bin
-  ${{VULKAN_SDK_PATH}}/Bin32
-  $ENV{{VULKAN_SDK}}/Bin/
-  $ENV{{VULKAN_SDK}}/Bin32/
-
+    ${{Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}}
+    /usr/bin
+    /usr/local/bin
+    ${{VULKAN_SDK_PATH}}/Bin
+    ${{VULKAN_SDK_PATH}}/Bin32
+    $ENV{{VULKAN_SDK}}/Bin/
+    $ENV{{VULKAN_SDK}}/Bin32/
 )
 
 # get all .vert and .frag files in shaders directory
 file(GLOB_RECURSE GLSL_SOURCE_FILES
-'${{PROJECT_SOURCE_DIR}}/shaders/*.frag'
-'${{PROJECT_SOURCE_DIR}}/shaders/*.vert'
+    "${{PROJECT_SOURCE_DIR}}/shaders/*.frag"
+    "${{PROJECT_SOURCE_DIR}}/shaders/*.vert"
 )
 
 foreach(GLSL ${{GLSL_SOURCE_FILES}})
-  get_filename_component(FILE_NAME ${{GLSL}} NAME)
-  set(SPIRV '${{PROJECT_SOURCE_DIR}}/shaders/${{FILE_NAME}}.spv')
-  add_custom_command(
-    OUTPUT ${{SPIRV}}
-    COMMAND ${{GLSL_VALIDATOR}} -V ${{GLSL}} -o ${{SPIRV}}
-    DEPENDS ${{GLSL}})
-  list(APPEND SPIRV_BINARY_FILES ${{SPIRV}})
+    get_filename_component(FILE_NAME ${{GLSL}} NAME)
+    set(SPIRV "${{PROJECT_SOURCE_DIR}}/shaders/${{FILE_NAME}}.spv")
+    add_custom_command(
+        OUTPUT ${{SPIRV}}
+        COMMAND ${{GLSL_VALIDATOR}} -V ${{GLSL}} -o ${{SPIRV}}
+        DEPENDS ${{GLSL}}
+    )
+    list(APPEND SPIRV_BINARY_FILES ${{SPIRV}})
 endforeach(GLSL)
 
 add_custom_target(
