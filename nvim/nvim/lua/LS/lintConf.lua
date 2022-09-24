e63d1cef240253e11dc7605b7477a3b52e8787eb
IZenithy <ahmedmohammed2429@gmail.com>
2022-09-24 08:35:36 +0300

    Nvim: Don't use cppcheck

--- nvim/nvim/lua/LS/lintConf.lua
+++ nvim/nvim/lua/LS/lintConf.lua
@@ -2,38 +2,11 @@ local utils = require('usr.utils')
 local lint = utils.include('lint')
 
 
--- cppcheck <= 1.84 doesn't support {column} so the start_col group is ambiguous
-local pattern = [[([^:]*):(%d*):([^:]*): %[([^%]\]*)%] ([^:]*): (.*)]]
-local groups = { 'file', 'lnum', 'col', 'code', 'severity', 'message' }
-local severity_map = {
-  ['error'] = vim.diagnostic.severity.ERROR,
-  ['warning'] = vim.diagnostic.severity.WARN,
-  ['performance'] = vim.diagnostic.severity.WARN,
-  ['style'] = vim.diagnostic.severity.INFO,
-  ['information'] = vim.diagnostic.severity.INFO,
-}
-
-require('lint').linters.custom_cppcheck = {
-cmd = 'cppcheck',
-  stdin = false,
-  args = {
-    '--enable=all',
-    '--language=c++',
-    '--inline-suppr',
-    '--quiet',
-    '--cppcheck-build-dir=../Build',
-    '--project=compile_commands.json',
-    '--template={file}:{line}:{column}: [{id}] {severity}: {message}',
-  },
-  stream = 'stderr',
-  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'cppcheck' }),
-}
 ----------------------------------------------------------------------
 --                               Glsl                               --
 
 lint.linters_by_ft =
 {
     glsl = {'glslc'},
-    cpp = {'custom_cppcheck'}
 }
 ----------------------------------------------------------------------
