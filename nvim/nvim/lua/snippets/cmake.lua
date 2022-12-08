ba2598ec2c48ff313a22d1ea3a3ec94a772538ed
IZenithy <ahmedmohammed2429@gmail.com>
2022-12-08 16:09:59 +0300

    Nvim: Edit shaders sinp

--- nvim/nvim/lua/snippets/cmake.lua
+++ nvim/nvim/lua/snippets/cmake.lua
@@ -114,15 +114,8 @@ local shaders = s('shaders',
 #                        Compile shader code                         #
 
 
-find_program(GLSL_VALIDATOR glslangValidator HINTS
-    ${{Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}}
-    /usr/bin
-    /usr/local/bin
-    ${{VULKAN_SDK_PATH}}/Bin
-    ${{VULKAN_SDK_PATH}}/Bin32
-    $ENV{{VULKAN_SDK}}/Bin/
-    $ENV{{VULKAN_SDK}}/Bin32/
-)
+find_package(Vulkan REQUIRED COMPONENTS glslc)
+find_program(GLSLC_EXECUTABLE NAMES glslc HINTS Vulkan::glslc)
 
 # get all .vert and .frag files in shaders directory
 file(GLOB_RECURSE GLSL_SOURCE_FILES
@@ -135,7 +128,7 @@ foreach(GLSL ${{GLSL_SOURCE_FILES}})
     set(SPIRV "${{PROJECT_SOURCE_DIR}}/shaders/${{FILE_NAME}}.spv")
     add_custom_command(
         OUTPUT ${{SPIRV}}
-        COMMAND ${{GLSL_VALIDATOR}} -V ${{GLSL}} -o ${{SPIRV}}
+        COMMAND ${{GLSLC_EXECUTABLE}} -O ${{GLSL}} -o ${{SPIRV}}
         DEPENDS ${{GLSL}}
     )
     list(APPEND SPIRV_BINARY_FILES ${{SPIRV}})
