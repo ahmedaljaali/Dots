2957dc8f5057d57327d16cc5c1bd407f6aa69325
IZenithy <ahmedmohammed2429@gmail.com>
2023-07-11 15:21:49 +0300

    Update

--- nvim/nvim/lua/rand/neovide.lua
+++ nvim/nvim/lua/rand/neovide.lua
@@ -20,7 +20,7 @@ if vim.fn.exists('g:neovide') == 1 then
     vim.g.neovide_cursor_vfx_particle_density=28.0
 
     -- Frame per second
-    vim.g.neovide_refresh_rate=20
+    vim.g.neovide_refresh_rate=60
 
     -- Shows a frame time graph in the upper left corner
     vim.g.neovide_profiler = false
@@ -28,5 +28,17 @@ if vim.fn.exists('g:neovide') == 1 then
     -- Determines how much the trail of the cursor lags behind the front edge
     vim.g.neovide_cursor_trail_length=5.0
 
+    -- Padding
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+
+    -- Hide the mouse
+    vim.g.neovide_hide_mouse_when_typing = true
+
+    vim.g.neovide_underline_automatic_scaling = true
+
+    vim.g.neovide_refresh_rate_idle = 5
 end
 ----------------------------------------------------------------------
