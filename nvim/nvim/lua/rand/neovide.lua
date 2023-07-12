3891e817b1ad6e6bb70a92ab823fc517a6a05572
IZenithy <ahmedmohammed2429@gmail.com>
2023-07-12 20:11:55 +0300

    Update neovide settings

--- nvim/nvim/lua/rand/neovide.lua
+++ nvim/nvim/lua/rand/neovide.lua
@@ -1,23 +1,22 @@
 ----------------------------------------------------------------------
 --        Everything here will be executed only with neovide        --
 
-if vim.fn.exists('g:neovide') == 1 then
+if vim.g.neovide then
 
     -- Start in fullscreen
     vim.g.neovide_fullscreen= true
 
-
     -- Determines the time it takes for the cursor to complete it's animation in seconds
-    vim.g.neovide_cursor_animation_length=0.35
+    vim.g.neovide_cursor_animation_length=0.7
 
     -- Cursor mode
     vim.g.neovide_cursor_vfx_mode = 'pixiedust'
 
     -- Sets the amount of time the generated particles should survive
-    vim.g.neovide_cursor_vfx_particle_lifetime=5
+    vim.g.neovide_cursor_vfx_particle_lifetime= 3
 
     -- Sets the number of generated particles
-    vim.g.neovide_cursor_vfx_particle_density=28.0
+    vim.g.neovide_cursor_vfx_particle_density= 50
 
     -- Frame per second
     vim.g.neovide_refresh_rate=60
@@ -40,5 +39,8 @@ if vim.fn.exists('g:neovide') == 1 then
     vim.g.neovide_underline_automatic_scaling = true
 
     vim.g.neovide_refresh_rate_idle = 5
+
+
+
 end
 ----------------------------------------------------------------------
